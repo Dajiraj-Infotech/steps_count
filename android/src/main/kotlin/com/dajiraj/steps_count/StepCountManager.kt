976b0c6c139088cb76d82d884130568b12cd01f5
Kumarpalsinh Rana <info@dajiraj.com>
2026-02-19 20:54:10 +0530

    Update StepCountManager.kt

--- android/src/main/kotlin/com/dajiraj/steps_count/StepCountManager.kt
+++ android/src/main/kotlin/com/dajiraj/steps_count/StepCountManager.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
